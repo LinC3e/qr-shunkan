5d7078be5bb59279e488095fef3cff99e441bbe8
LinC3e <ivancostilla17@gmail.com>
2026-03-09 18:23:48 -0300

    upd: url validation in service qr

--- internal/qr/service.go
+++ internal/qr/service.go
@@ -3,8 +3,16 @@ package qr
 import (
 	"context"
 	"errors"
+	"net/url"
+	"strings"
 
 	"github.com/google/uuid"
+)
+
+var (
+	ErrEmptyContent   = errors.New("the content cannot be empty")
+	ErrInvalidURL     = errors.New("valid URL: (ej: https://ejemplo.com)")
+	ErrNoScheme       = errors.New("URL protocol (http o https)")
 )
 
 type Service struct {
@@ -16,9 +24,31 @@ func NewService(repo Repository) *Service {
 }
 
 func (s *Service) CreateQR(ctx context.Context, content string) (*QR, error) {
+	content = strings.TrimSpace(content)
 	if content == "" {
-		return nil, errors.New("content is required")
+		return nil, ErrEmptyContent
 	}
+
+	parsedURL, err := url.ParseRequestURI(content)
+	if err != nil {
+		if strings.HasPrefix(content, "www.") || strings.Contains(content, ".") {
+			// https:// auto
+			testWithScheme := "https://" + content
+			parsedURL, err = url.ParseRequestURI(testWithScheme)
+			if err == nil {
+				content = testWithScheme
+			}
+		}
+
+		if err != nil {
+			return nil, ErrInvalidURL
+		}
+	}
+
+	if parsedURL.Scheme != "http" && parsedURL.Scheme != "https" {
+		return nil, ErrNoScheme
+	}
+
 	return s.repo.Create(ctx, content)
 }
 
